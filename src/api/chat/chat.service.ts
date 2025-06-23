a26c29044a37ace560d8ef268c62ee061a48966e
Dor <dorhakim100@gmail.com>
2025-06-23 17:51:02 +0300

    Add aggregate messages and user in chat

--- src/api/chat/chat.service.ts
+++ src/api/chat/chat.service.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from 'mongodb'
 import { dbService } from '../../db/mongo/db.service'
 import { logger } from '@/services/logger.service'
+import { UserService } from '../user/user.service'
 
 export interface Chat {
   _id?: string
@@ -23,10 +24,71 @@ export async function getChatByRoomIdService(
   roomId: string
 ): Promise<Chat | null> {
   try {
+    const criteria = { roomId }
+
+    const aggregationPipeline = [
+      { $match: criteria }, // Match chats based on criteria
+
+      {
+        $addFields: {
+          // Convert each user id in the users array from a string to an ObjectId.
+
+          messages: {
+            $map: {
+              input: '$messages',
+              as: 'messageId',
+              in: { $toObjectId: '$$messageId' },
+            },
+          },
+        },
+      },
+
+      {
+        $lookup: {
+          from: MESSAGE_COLLECTION, // The collection to join with for message details
+          localField: 'messages', // The converted latestMessage ObjectId
+          foreignField: '_id', // The _id field in the message collection
+          // as: 'messageDetails', // Output array with the matching message document(s)
+          as: 'messages', // Output array with the matching message document(s)
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+
+          // messageDetails: 1,
+          messages: 1,
+          roomId: 1,
+        },
+      },
+    ]
+
     const collection = await dbService.getCollection(CHAT_COLLECTION)
-    const chat = await collection.findOne({ roomId })
-    if (!chat) return null
-    chat._id = chat._id.toString()
+    let chat = await collection.aggregate(aggregationPipeline).toArray()
+    chat = chat[0]
+
+    const messagesWithUsers = await Promise.all(
+      chat.messages.map(async (message: Message) => {
+        const userId = message.fromId
+        const user = await UserService.getById(userId)
+        const userToReturn = {
+          email: user?.email,
+          fullname: user?.fullname,
+          id: user?.id,
+          imgUrl: user?.imgUrl,
+        }
+        return { ...message, user: userToReturn }
+      })
+    )
+    chat.messages = messagesWithUsers
+
+    // if (!chats || chats.length === 0) {
+    //   return null // Return null if no item is found
+    // }
+
+    // const chat = await collection.findOne({ roomId })
+    // if (!chat) return null
+    // chat._id = chat._id.toString()
     return chat as Chat
   } catch (err) {
     throw err
